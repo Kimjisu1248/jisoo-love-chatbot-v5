71b59c451787d3233529a07776ce312343901e39
Kimjisu1248 <kjs021123@naver.com>
2025-05-23 16:40:16 +0900

    Add files via upload

---
+++ src/pages/Result.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Result() {
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <h2>MBTI: ENFP</h2>
+      <img src="/images/ENFP.png" alt="ENFP 캐릭터" style={{ width: 100 }} />
+      <p><strong>이름:</strong> 민정오빠</p>
+      <p><strong>말투:</strong> 장난기 + 다정함</p>
+      <p><strong>호감도:</strong> 💘💘💘💘💘 (80/100)</p>
+    </div>
+  );
+}
